b72d067f3b143fe746107a5fac6a9fb89cc35025
nebunite <nebunite4843@gmail.com>
2023-05-31 01:58:33 -0700

    i dunno it works lol

--- game.js
+++ game.js
@@ -65,12 +65,54 @@ Game.Scene = {
         "Corporation Screen",
         {
             enterScene: function (corp) {
-                this.Sprites = [
-                    new Soleng.Graphics.Shape.Value(
-                        10, 10, corp, 'funds', 'white'
-                    )
-                ]
+                this.corp = corp
+            },
+            updateScene: function () {
+                let corp = this.corp
                 
+                let departments = corp.Departments.length
+                let resources   = corp.Resources.length
+                let grid = new Soleng.Graphics.Composite.Grid(
+                    84,
+                    10 + departments + resources
+                )
+                grid.drawBox(0,0,grid.width, grid.height)
+                // Box out Departments
+                grid.drawBox(0,3,grid.width, 3)
+                // Box out Resources
+                grid.drawBox(0,6+departments, grid.width, 3)
+                
+                grid.writeText(2, 1, corp.name)
+                grid.writeText(2, 2, "Funds: " + corp.funds)
+                grid.writeText(41, 2, "Operation time: " + corp.timePassed + " days")
+                grid.writeText(2,4, "Department          | Productivity | Budget | Morale | Manager Skill | Operation")
+                for (let i = 0; i < corp.Departments.length; i++) {
+                    let department = corp.Departments[i]
+                    let dN = department.name
+                    let pr = department.productivity.toString()
+                    let bd = department.budget.toString()
+                    let mr = department.morale.toString()
+                    let mn = department.manager.toString()
+                    grid.writeText(2,  6+i, dN)
+                    grid.writeText(22, 6+i, "| " + pr)
+                    grid.writeText(37, 6+i, "| " + bd)
+                    grid.writeText(46, 6+i, "| " + mr)
+                    grid.writeText(55, 6+i, "| " + mn)
+                    grid.writeText(71, 6+i, "| " + "Active")
+                }
+                grid.writeText(2, 7+departments, "Resources           | Amount       | Market Value | Volume")
+                for (let i = 0; i < corp.Resources.length; i++) {
+                    let resource = corp.Resources[0]
+                    let rn = resource.name
+                    let ra = resource.amount.toString()
+                    let rm = resource.marketValue.toString()
+                    let rv = resource.volume.toString()
+                    grid.writeText(2,  9+departments+i, rn)
+                    grid.writeText(22, 9+departments+i, "| " + ra + "m^3")
+                    grid.writeText(37, 9+departments+i, "| " + rm + "/Unit")
+                    grid.writeText(52, 9+departments+i, "| " + rv + "m^3/Unit")
+                }
+                this.Sprites = [ grid ]
             }
         }
     )
@@ -85,8 +127,6 @@ let corp = new Soleng.Economics.Corporations.Subsidiary()
 
 corp.operate(100)
 
-console.log(corp.funds)
-console.log(corp.log.Log[3])
-
 Game.Scene.CorpGeneral.enterScene(corp)
+Game.Scene.CorpGeneral.updateScene()
 Game.Scene.CorpGeneral.renderScene()
